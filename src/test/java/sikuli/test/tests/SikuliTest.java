ca713014235d118269f36bb9abe9d2ed6151ed87
user <user@mail.com>
2017-12-13 21:42:35 +0200

    add SikuliTest

---
+++ src/test/java/sikuli/test/tests/SikuliTest.java
@@ -0,0 +1,12 @@
+package sikuli.test.tests;
+
+import webdriver.BaseTest;
+
+public class SikuliTest extends BaseTest {
+    @Override
+    public void runTest() {
+
+        logger.step(1);
+
+    }
+}
