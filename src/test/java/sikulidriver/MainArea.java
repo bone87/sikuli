9449cb6aae6b9e6f16821816fefb6a703e582bcd
bone <87bone@gmail.com>
2017-12-16 17:37:45 +0300

    ref

--- src/test/java/sikulidriver/MainArea.java
+++ src/test/java/sikulidriver/MainArea.java
@@ -3,15 +3,17 @@ package sikulidriver;
 import org.testng.Assert;
 
 public class MainArea {
-    private String path_to_pic_folder;
-    private SikuliElement tableItem = new SikuliElement(path_to_pic_folder.concat("item.png"), "item");
-    private SikuliElement screenCenter = new SikuliElement(path_to_pic_folder.concat("center.png"), "screen center");
-    private SikuliElement btnClose = new SikuliElement(path_to_pic_folder.concat("btnClose.png"), "button close");
-    private SikuliElement itemOnWorkSpace = new SikuliElement(path_to_pic_folder.concat("verifyDrop.png"), "item on work space");
+    private SikuliElement tableItem;
+    private SikuliElement screenCenter;
+    private SikuliElement btnClose;
+    private SikuliElement itemOnWorkSpace;
 
     public MainArea(String path_to_pic_folder) {
         Assert.assertNotNull(path_to_pic_folder, "Missing the path to pic folder");
-        this.path_to_pic_folder = path_to_pic_folder;
+        tableItem = new SikuliElement(path_to_pic_folder.concat("item.png"), "item");
+        screenCenter = new SikuliElement(path_to_pic_folder.concat("center.png"), "screen center");
+        btnClose = new SikuliElement(path_to_pic_folder.concat("btnClose.png"), "button close");
+        itemOnWorkSpace = new SikuliElement(path_to_pic_folder.concat("verifyDrop.png"), "item on work space");
     }
 
     /**
